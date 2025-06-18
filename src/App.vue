2c530a43d80443f38b3349ccf37778c4f57b723b
xchnly <hendryhuang70@gmail.com>
2025-06-18 16:35:42 +0700

    Update1

--- src/App.vue
+++ src/App.vue
@@ -111,7 +111,7 @@ export default {
         { name: '进龙', initials: 'JL', position: '仓库' },
         { name: '豪俊', initials: 'HJ', position: 'PMC' },
         { name: '芳芳', initials: 'FF', position: 'PMC' },
-        { name: '资阳', initials: 'ZY', position: '船务' },
+        { name: '资杨', initials: 'ZY', position: '船务' },
         { name: 'Arthur', initials: 'AR', position: '船务' },
       ]
     }
